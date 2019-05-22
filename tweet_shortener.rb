cf9a93b987181630aaf39393f696dc3a34252065
Reina Ewing <reina.castro3@gmail.com>
2019-05-22 18:48:25 +0000

    Automatically backed up by Learn

--- tweet_shortener.rb
+++ tweet_shortener.rb
@@ -1,1 +1,41 @@
 # Write your code here.
+
+def dictionary
+  hash = {
+    "hello" => "hi",
+    "to" => "2",
+    "two" => "2",
+    "too" => "2",
+    "For" => "4",
+    "for" => "4",
+    "four" => "4",
+    "be" => "b",
+    "you" => "u",
+    "at" => "@",
+    "and" => "&"
+  }
+end 
+
+def word_substituter(string)
+  string.split(" ").each { |word| word.replace dictionary[word] if dictionary[word] }.join(" ")
+end 
+
+def bulk_tweet_shortener(array)
+  array.each { |i| puts word_substituter(i) }
+end 
+
+def selective_tweet_shortener(array)
+  if array.length <= 140 
+    return array 
+  else 
+    word_substituter(array)
+  end 
+end 
+
+
+def shortened_tweet_truncator 
+
+
+
+
+
