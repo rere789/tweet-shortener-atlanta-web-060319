7ee26a6aaab810ac97e702a074c68ba1bc0233f8
Reina Ewing <reina.castro3@gmail.com>
2019-05-22 19:01:47 +0000

    Automatically backed up by Learn

--- tweet_shortener.rb
+++ tweet_shortener.rb
@@ -35,7 +35,7 @@ end
 
 def shortened_tweet_truncator(array)
       new_array = selective_tweet_shortener(array)
-      if new_array.length > 140 
+      if new_array.length >= 140 
         new_array[0..135] << "..."
       else 
         new_array 
